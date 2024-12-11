c722a8e163611676fb49454198072301cd93afee
Abhay Mishra <abhaym1205@gmail.com>
2024-12-12 00:03:45 +0530

    solved tailwind plugin issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
@@ -12,5 +13,5 @@ export default {
 			colors: {}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 }
